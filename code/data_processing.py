fix: next action calculation

code/data_processing.py
@@ -1,16 +1,17 @@
 import numpy as np
+from collections import deque
+import random
 
-
-def random_slice(array, slice_size):
-    # Ensure the slice size is not larger than the array
-    if slice_size > len(array):
-        raise ValueError("Slice size cannot be larger than the array length.")
+def random_slice(deque_obj, slice_size: int):
+    # Ensure the slice size is not larger than the deque
+    if slice_size > len(deque_obj):
+        raise ValueError("Slice size cannot be larger than the deque length.")
 
     # Calculate a random starting index
-    start_index = np.random.randint(0, len(array) - slice_size + 1)
+    start_index = random.randint(0, len(deque_obj) - slice_size)
 
     # Obtain the slice
-    return array[start_index:start_index + slice_size]
+    return [deque_obj[i] for i in range(start_index, start_index + slice_size)]
 def read_text_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()

code/main.py
@@ -50,7 +50,7 @@ class QLearningAgent:
             curr_col, curr_dist = self.color_mapping[state[0]], self.distance_mapping[state[1]]
             next_col, next_dist = self.color_mapping[next_state[0]], self.distance_mapping[next_state[1]]
             action = self.action_mapping[action]
-            best_next_action = np.argmax(self.Q_table[next_col , next_dist])
+            best_next_action = np.argmax(self.Q_table[next_col , next_dist, : , curr_col , curr_dist])
             td_target = reward + self.discount_factor * self.Q_table[next_col , next_dist, best_next_action , curr_col , curr_dist]
             td_error = td_target - self.Q_table[curr_col, curr_dist, action , prev_col , prev_dist]
             self.Q_table[curr_col, curr_dist, action , prev_col , prev_dist] += self.learning_rate * td_error