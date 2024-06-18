1fb888e171624f3529dbcd0819c30dd7dad10124
yessmine <chabchoub.yessmine@gmail.com>
2024-06-18 13:40:45 +0200

    added fct to convert npy to csv

--- code/test.py
+++ code/test.py
@@ -5,12 +5,20 @@ from main import QLearningAgent, color_mapping, distance_mapping, action_mapping
 import os
 import numpy as np
 logging.basicConfig(level=logging.INFO)
+import pandas as pd
 
+def convert_q_table_to_csv(q_table_npy_path, csv_path):
+    Q_table = np.load(q_table_npy_path)
+    flattened_Q_table = Q_table.reshape(-1, Q_table.shape[-1]).astype(float)
+    df = pd.DataFrame(flattened_Q_table)
+    df.to_csv(csv_path, index=False)
+    print(f"Q-table successfully converted to CSV and saved at {csv_path}")
+    
 def get_best_action(Q_table, current_state, previous_state):
     curr_col, curr_dist = current_state
     prev_col, prev_dist = previous_state
-    best_action = np.argmax(Q_table[curr_col , curr_dist, : , prev_col , prev_dist])
-    logging.info(f"Best action for state {sample_state} and previous state {sample_prev_state} is: {best_action}")
+    best_action = np.argmax(Q_table[curr_col , curr_dist, : , prev_col , prev_dist]) #ToDo not sure
+    logging.info(f"Best action for state {sample_state} and previous state {sample_prev_state} gis: {best_action}")
     #return best_action
 
 def test_Qvalue(Q_table, sample_state, sample_action, sample_prev_state):
@@ -33,7 +41,7 @@ def import_Q_table():
 test_curr_color = 'red'
 test_curr_distance = 'dis_4'
 test_action = 'forward'
-test_prev_color = 'white'
+test_prev_color = 'blue'
 test_prev_distance = 'dis_3'
 
 ## Compute the Q-value for the sample state and action
@@ -43,3 +51,5 @@ sample_prev_state = (color_mapping[test_prev_color], distance_mapping[test_curr_
 sample_action = action_mapping[test_action]
 test_Qvalue(Q_table,sample_state, sample_action, sample_prev_state)
 get_best_action(Q_table,sample_state, sample_prev_state)
+##
+##convert_q_table_to_csv('Q_table.npy', 'Q_table.csv')
