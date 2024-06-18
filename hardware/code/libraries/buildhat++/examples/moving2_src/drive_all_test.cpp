a10f10f7ae33a83a670b3c7ae6e67769416b3416
Yamar <yamapimoukam.1@campus.tu-berlin.de>
2024-06-18 18:22:14 +0200

    test reward

--- hardware/code/libraries/buildhat++/examples/moving2_src/drive_all_test.cpp
+++ hardware/code/libraries/buildhat++/examples/moving2_src/drive_all_test.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <fstream>
+#include <Python.h>
 #include <cstdlib> // For getenv
 #include "functions/Drive.hpp"
 #include "modules/sensors/ColorSensor.hpp"
@@ -21,6 +22,142 @@ std::ofstream outputFile;
 
     std::string lastcolor = "white";
     std::string lastdistance = "dist_0";
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+//// use python to get goal_achieved  and calculate_reward
+
+
+
+
+// Function to call the Python function goal_achieved
+bool call_goal_achieved(const std::string& color, const std::string& distance) {
+    // Initialize the Python interpreter
+    Py_Initialize();
+
+    // Set the Python script file name
+    const char* script_name = "env";
+    
+    // Convert the file name to a Python string
+    PyObject* pName = PyUnicode_DecodeFSDefault(script_name);
+
+    // Import the Python module
+    PyObject* pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    bool result = false;
+
+    if (pModule != nullptr) {
+        // Get the function from the module
+        PyObject* pFunc = PyObject_GetAttrString(pModule, "goal_achieved");
+
+        // Check if the function is callable
+        if (pFunc && PyCallable_Check(pFunc)) {
+            // Create the argument tuple
+            PyObject* pArgs = PyTuple_Pack(1, PyTuple_Pack(2, PyUnicode_FromString(color.c_str()), PyUnicode_FromString(distance.c_str())));
+
+            // Call the function
+            PyObject* pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+
+            if (pValue != nullptr) {
+                result = PyObject_IsTrue(pValue);
+                Py_DECREF(pValue);
+            } else {
+                PyErr_Print();
+                std::cerr << "Call failed" << std::endl;
+            }
+        } else {
+            if (PyErr_Occurred()) PyErr_Print();
+            std::cerr << "Cannot find function 'goal_achieved'" << std::endl;
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    } else {
+        PyErr_Print();
+        std::cerr << "Failed to load '" << script_name << "'" << std::endl;
+    }
+
+    // Finalize the Python interpreter
+    Py_Finalize();
+
+    return result;
+}
+
+// Function to call the Python function calculate_reward
+double call_calculate_reward(const std::string& current_color, const std::string& current_distance, const std::string& next_color, const std::string& next_distance, bool goal) {
+    // Initialize the Python interpreter
+    Py_Initialize();
+
+    // Set the Python script file name
+    const char* script_name = "env";
+    
+    // Convert the file name to a Python string
+    PyObject* pName = PyUnicode_DecodeFSDefault(script_name);
+
+    // Import the Python module
+    PyObject* pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    double result = 0.0;
+
+    if (pModule != nullptr) {
+        // Get the function from the module
+        PyObject* pFunc = PyObject_GetAttrString(pModule, "calculate_reward");
+
+        // Check if the function is callable
+        if (pFunc && PyCallable_Check(pFunc)) {
+            // Create the argument tuple
+            PyObject* pArgs = PyTuple_Pack(5, 
+                                           PyUnicode_FromString(current_color.c_str()), 
+                                           PyUnicode_FromString(current_distance.c_str()), 
+                                           PyUnicode_FromString(next_color.c_str()), 
+                                           PyUnicode_FromString(next_distance.c_str()), 
+                                           goal ? Py_True : Py_False);
+
+            // Call the function
+            PyObject* pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+
+            if (pValue != nullptr) {
+                result = PyFloat_AsDouble(pValue);
+                Py_DECREF(pValue);
+            } else {
+                PyErr_Print();
+                std::cerr << "Call failed" << std::endl;
+            }
+        } else {
+            if (PyErr_Occurred()) PyErr_Print();
+            std::cerr << "Cannot find function 'calculate_reward'" << std::endl;
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    } else {
+        PyErr_Print();
+        std::cerr << "Failed to load '" << script_name << "'" << std::endl;
+    }
+
+    // Finalize the Python interpreter
+    Py_Finalize();
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
 
 
@@ -168,9 +305,21 @@ void execute_command(int command, double angle, Drive& drive) {
         std::this_thread::sleep_for(std::chrono::milliseconds{1000});
 
 
+///////////// use python code
+///////
+    std::string current_color = lastcolor;
+    std::string current_distance = lastdistance;
+    std::string next_color = colorName;
+    std::string next_distance = distanceRange;
+    bool achieved = call_goal_achieved(next_color, next_distance);
+    //std::cout << "Goal achieved: " << (achieved ? "Yes" : "No") << std::endl;
+
+    double reward = call_calculate_reward(current_color, current_distance, next_color, next_distance, achieved);
+    //std::cout << "Reward: " << reward << std::endl;
+//////
 
     std::string prevstate =   "(" + lastcolor + ", " + lastdistance + ")" ;
-    outputFile << "(" << prevstate << ", " << actionName << ", (" << colorName << ", " << distanceRange << "),++" << ")" << std::endl;
+    outputFile << "(" << prevstate << ", " << actionName << ", (" << colorName << ", " << distanceRange << ")," << reward << "," << achieved ? "True" : "False"<< ")" << std::endl;
 
 
         lastdistance = distanceRange; 
