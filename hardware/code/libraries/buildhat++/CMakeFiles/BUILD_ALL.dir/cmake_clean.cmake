c61a0a9d255795d7081cf3a7eee6a6bb928c1626
Robert Wilde <robert.wilde@campus.tu-berlin.de>
2024-06-18 17:50:28 +0200

    gitignore Fix

--- hardware/code/libraries/buildhat++/CMakeFiles/BUILD_ALL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BUILD_ALL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BUILD_ALL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- hardware/code/libraries/buildhat++/CMakeFiles/drive_all_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for drive_all_test.
-# This may be replaced when dependencies are built.
