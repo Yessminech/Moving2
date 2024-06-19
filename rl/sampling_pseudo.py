d761cf84275f57809df95e7e8756fe2678f0fbc6
yessmine <chabchoub.yessmine@gmail.com>
2024-06-19 12:47:51 +0200

    commented out unused code

--- rl/sampling_pseudo.py
+++ rl/sampling_pseudo.py
@@ -1,31 +1,31 @@
-import time
+# import time
 
-# Define data collection parameters
-num_episodes = 100
-episode_length = 100
-data = []
+# # Define data collection parameters
+# num_episodes = 100
+# episode_length = 100
+# data = []
 
-# Run random policy and collect data
-for episode in range(num_episodes):
-    for _ in range(episode_length):
-        # Randomly choose action
-        action = np.random.choice(num_actions)
+# # Run random policy and collect data
+# for episode in range(num_episodes):
+#     for _ in range(episode_length):
+#         # Randomly choose action
+#         action = np.random.choice(num_actions)
         
-        # Execute action on hardware (e.g., control motors)
-        execute_action(action)
+#         # Execute action on hardware (e.g., control motors)
+#         execute_action(action)
         
-        # Read sensor data
-        state = read_sensor_data()
+#         # Read sensor data
+#         state = read_sensor_data()
         
-        # Simulate reward (for example purposes)
-        reward = simulate_reward(state)
+#         # Simulate reward (for example purposes)
+#         reward = simulate_reward(state)
         
-        # Wait for a short time (optional)
-        time.sleep(0.1)
+#         # Wait for a short time (optional)
+#         time.sleep(0.1)
         
-        # Record data
-        next_state = read_sensor_data()
-        data.append((state, action, reward, next_state))
+#         # Record data
+#         next_state = read_sensor_data()
+#         data.append((state, action, reward, next_state))
 
-# Save collected data to a file
-np.save('offline_data.npy', data)
+# # Save collected data to a file
+# np.save('offline_data.npy', data)
